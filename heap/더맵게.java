fe166dcb14cbc8ff04ae6cad0cb0ba4876d77ff1
limminjeong98 <limminjeong98@naver.com>
2022-02-13 12:48:42 +0900

    Queue -> Queue<Integer>

--- heap/더맵게.java
+++ heap/더맵게.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        Queue pq = new PriorityQueue();
-        for (int i : scoville) {
+        Queue<Integer> pq = new PriorityQueue<>();
+        for (int i: scoville) {
             pq.offer(i);
         }
-        if ((int) pq.peek() >= K) {
+        if (pq.peek() >= K) {
             return answer;
         }
         answer = 1;
-        while (pq.size() >= 2) {
-            int food = (int) pq.poll() + (int) pq.poll() * 2;
+        while (pq.size() >= 2){
+            int food = pq.poll() + pq.poll() * 2;
             pq.offer(food);
-            if ((int) pq.peek() >= K) {
+            if (pq.peek() >= K) {
                 return answer;
             }
             answer++;
