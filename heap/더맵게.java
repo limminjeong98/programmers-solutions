6a607c7ccc08a24bf1df2d8f862201cf24163b7c
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-09-18 12:22:33 +0900

    Update 더맵게.java

--- heap/더맵게.java
+++ heap/더맵게.java
@@ -1,24 +1,26 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+
+        Arrays.sort(scoville);
+        if (scoville[0] >= K) return 0;
+        
+        // 이렇게하면 에러남
+        // Queue<Integer> heap = new PriorityQueue<>(Arrays.asList(Arrays.stream(scoville).boxed().toArray()));
+        Queue<Integer> heap = new PriorityQueue(Arrays.asList(Arrays.stream(scoville).boxed().toArray()));
+        
         int answer = 0;
-        Queue<Integer> pq = new PriorityQueue<>();
-        for (int i: scoville) {
-            pq.offer(i);
-        }
-        if (pq.peek() >= K) {
-            return answer;
-        }
-        answer = 1;
-        while (pq.size() >= 2){
-            int food = pq.poll() + pq.poll() * 2;
-            pq.offer(food);
-            if (pq.peek() >= K) {
-                return answer;
-            }
+        
+        while (heap.size() > 1) {
             answer++;
+            int score = heap.poll() + heap.poll() * 2;
+            heap.offer(score);
+            if (heap.peek() >= K) return answer;
         }
+        
         return -1;
     }
 }
