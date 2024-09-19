7b21f8562e7ced6c81af62ea1e5038ddd343e930
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-09-19 09:57:11 +0900

    Update 가장큰수.java

--- sort/가장큰수.java
+++ sort/가장큰수.java
@@ -1,26 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String[] arr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
+        // String answer = "";
+        int n = numbers.length;
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = String.valueOf(numbers[i]);
         }
 
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return ((o2 + o1).compareTo(o1 + o2));
-            }
-        });
-
-        if (arr[0].equals("0")) {
-            return "0";
-        }
-        for (String s : arr) {
-            answer += s;
-        }
-        return answer;
+        // Arrays.sort(arr, new Comparator<String>() {
+        //     @Override
+        //     public int compare(String o1, String o2) {
+        //         return ((o2 + o1).compareTo(o1 + o2));
+        //     }
+        // });
+        Arrays.sort(arr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        
+        if (arr[0].equals("0")) return "0";
+        return String.join("", arr);
     }
 }
