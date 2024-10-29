2cfcfbab607268cc5a50989272bbfb4599599e3e
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-10-29 14:13:01 +0900

    Create k진수에서_소수_개수_구하기.java

---
+++ 2022_kakao_blind_recruitment/k진수에서_소수_개수_구하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String str = Integer.toString(n, k);
+        String[] arr = str.split("0");
+        for (String s : arr) {
+            if (s.equals("")) continue;
+            // n이 최대 1,000,000인 경우 3진수로 변환했을 때 int 범위 벗어남
+            if (isPrime(Long.parseLong(s))) answer++;
+        }
+        
+        return answer;
+    }
+    
+    public boolean isPrime(long num) {
+        if (num == 1) return false;
+        if (num == 2 || num == 3) return true;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
