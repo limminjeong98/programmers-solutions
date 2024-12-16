7d821248bdb26dcac631c5ad2dea3964e0ab65bd
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-12-16 17:43:23 +0900

    Update 타겟넘버.java

--- dfs_bfs/타겟넘버.java
+++ dfs_bfs/타겟넘버.java
@@ -1,20 +1,19 @@
 class Solution {
-    public static int answer = 0;
-
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, numbers.length, 0, target);
-        return answer;
+        // class 변수로 선언 안하고 메서드 파라미터로 answer 전달하기 위해
+        int[] answer = new int[1];
+        dfs(numbers, target, answer, numbers.length, 0, 0);
+        return answer[0];
     }
-
-    public void dfs(int[] nums, int cnt, int n, int sum, int target) {
-        if (cnt == n) {
+    
+    public void dfs(int[] numbers, int target, int[] answer, int n, int count, int sum) {
+        if (count == n) {
             if (sum == target) {
-                answer++;
+                answer[0]++;
             }
-        } else {
-            dfs(nums, cnt + 1, n, sum + nums[cnt], target);
-            dfs(nums, cnt + 1, n, sum - nums[cnt], target);
-
+            return;
         }
+        dfs(numbers, target, answer, n, count + 1, sum + numbers[count]);
+        dfs(numbers, target, answer, n, count + 1, sum - numbers[count]);
     }
 }
