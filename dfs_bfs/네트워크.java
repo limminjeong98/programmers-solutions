851fc85e7ef179dfaceee630dc50a22102891a90
minjeong <48610633+limminjeong98@users.noreply.github.com>
2024-12-16 17:33:40 +0900

    Update 네트워크.java

--- dfs_bfs/네트워크.java
+++ dfs_bfs/네트워크.java
@@ -1,24 +1,25 @@
 class Solution {
-    public static int answer = 0;
-    public static boolean[] visited;
-
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        int answer = 0;
+        
+        boolean[] visited = new boolean[n];
+        
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(n, i, computers, visited);
-                answer++;
-            }
+            if (visited[i]) continue;
+            dfs(n, i, computers, visited);
+            answer++;
         }
+        
         return answer;
     }
-
-    public void dfs(int n, int cur, int[][] computers, boolean[] visited) {
-        for (int i = 0; i < n; i++) {
-            if (computers[cur][i] == 1 && !visited[i]) {
-                visited[i] = true;
-                dfs(n, i, computers, visited);
+    
+    public void dfs(int n, int i, int[][] computers, boolean[] visited) {
+        visited[i] = true;
+        
+        for (int j = 0; j < n; j++) {
+            if (visited[j]) continue;
+            if (computers[i][j] == 1) {
+                dfs(n, j, computers, visited);
             }
         }
     }
